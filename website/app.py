24e5e934ed890bc18e75df9081d10ec94f6a9bef
Oğuzhan Kalelioğlu <oguzhankalelioglu@protonmail.com>
2023-03-30 10:59:53 +0300

    second commit

--- website/app.py
+++ website/app.py
@@ -1,14 +1,24 @@
-from flask import Flask, render_template , request
+from flask import Flask, render_template , request , models
 
 __all__ = ["create_app"]
 
+product_id = 1
 
 def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('settings.py')
 
-    @app.route("/")
-    def index():
-         return render_template('index.html')
+ #   @app.route("/")
+ #   def index():
+ #        return render_template('index.html') 
+
+    @app.route('/')
+    def products_list():
+        return render_template('index.html', products=models.Product.all())
+
+    @app.route('/product_<int:product_id>/')
+    def product_details():
+        product = models.Product.get_or_404(id=product_id)
+        return render_template('product.html', product=product)
 
     return app
